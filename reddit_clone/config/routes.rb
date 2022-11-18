147c4ccff7541ef758c49a23e9830f03a8d967e9
Patrick Choi <ptrckchoi@gmail.com>
2022-11-18 09:53:29 -0800

    finished spire and crlll

---
+++ reddit_clone/config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  resources :users, only: [:create, :new, :show, :index]
+  resource :session, only: [:new, :create, :destroy]
+end
